d328775a5d4ec84fbc28a88175ce1a80780ed1a9
Fábio Silva <fabio.fernando.osilva@gmail.com>
2017-11-22 22:20:01 -0300

    Remove JS code from HTML

---
+++ js/init.js
@@ -0,0 +1,4 @@
+var group1 = ["PDT", "PT do B", "PSB", "AVANTE", "PSTU", "PCB", "NOVO"];
+var group2 = ["PC do B", "PROS", "PT", "REDE", "PSOL", "PRTB"];
+var group3 = ["PEN", "PPS", "PV", "PSL", "PMN", "PTC", "PPL", "PCO"];
+var group4 = ["PR", "PRB", "PMDB", "PTB", "PP", "DEM", "PSD", "PSDC", "SD", "PHS", "PODE", "PSC", "PSDB", "PRP", "PMB"];
